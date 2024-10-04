eddc62d47cfb0756c0ad650fbdb5f6efba2ae5f8
Thanh2923 <thanhnv2923@gmail.com>
2024-10-04 19:35:48 +0700

    upload

---
+++ youngtech-fe/src/app/page.tsx
@@ -0,0 +1,12 @@
+import AppCategories from "@/components/users/page/app.categories";
+import AppMainHome from "@/components/users/home/app.mainHome";
+
+
+export default function Home() {
+  return (
+  <>
+    <AppCategories/>
+    <AppMainHome/>
+  </>
+  );
+}
