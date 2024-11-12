7f50de4d19e87bc25b6f7d033d60ff7ed2ea00fe
ylasao <rencooking09202@gmail.com>
2024-11-12 08:17:03 +0700

    fix conflict

--- youngtech-fe/src/app/page.tsx
+++ youngtech-fe/src/app/page.tsx
@@ -1,16 +1,14 @@
-import Link from "next/link";
-
+import Carousels from "../components/carousel/Carousel";
+import { MainHome } from "../components/MainHome";
 export default function Home() {
   return (
     <>
-      <div className="pl-10 pr-10 pt-10 pb-10">
-        <Link
-          href="/admin"
-          className="mt-6 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
-        >
-          Vào trang quản trị
-        </Link>
-      </div> 
+      <div className="w-full banner">
+        <Carousels />
+      </div>
+      <div className="w-[90%] m-auto">
+        <MainHome />
+      </div>
     </>
   );
 }
