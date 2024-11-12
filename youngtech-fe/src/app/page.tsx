1f41818260d0df8ca5b5d66971e0e39add3c2eeb
ylasao <rencooking09202@gmail.com>
2024-11-12 08:10:21 +0700

    fix conflicting

--- youngtech-fe/src/app/page.tsx
+++ youngtech-fe/src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Link from "next/link";
 
 export default function Home() {
@@ -11,17 +10,7 @@ export default function Home() {
         >
           Vào trang quản trị
         </Link>
-      </div>
+      </div> 
     </>
-=======
-import Image from "next/image";
-import Header from "./admin/header";
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Header />
-      <h1>hello word </h1>
-    </div>
->>>>>>> 1022410 (test)
   );
 }
