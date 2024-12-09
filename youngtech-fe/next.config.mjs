c075d55430d53b37d953d7b52450c9e2099bb40c
Thanhnv2923 <thanhnv2923@gmail.com>
2024-12-09 16:07:55 +0700

    fide upload image

--- youngtech-fe/next.config.mjs
+++ youngtech-fe/next.config.mjs
@@ -1,7 +1,9 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    images: {
+        domains: ['res.cloudinary.com'], // Thêm domain của Cloudinary vào đây
+      },
 };
 
 export default nextConfig;
