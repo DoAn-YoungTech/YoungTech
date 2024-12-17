8344870e9457f52950f6dde919a3967318ebc596
quan09012001 <nhq.it.vn@gmail.com>
2024-12-18 02:50:22 +0700

    kcj

--- youngtech-be/src/routes/supplierRoutes.js
+++ youngtech-be/src/routes/supplierRoutes.js
@@ -3,24 +3,24 @@ const supplierController = require("../controllers/supplierController");
 const supplierRoutes = express.Router();
 const middlewareController = require('../controllers/middlewareController');
 supplierRoutes.get("/", 
-    middlewareController.verifyToken,
-    middlewareController.verifyTokenAndRole(['storekeeper']),
+    // middlewareController.verifyToken,
+    // middlewareController.verifyTokenAndRole(['storekeeper']),
     supplierController.getAllSuppliers);
 supplierRoutes.post("/", 
-    middlewareController.verifyToken,
-    middlewareController.verifyTokenAndRole(['storekeeper']),
+    // middlewareController.verifyToken,
+    // middlewareController.verifyTokenAndRole(['storekeeper']),
     supplierController.createSupplier);
 supplierRoutes.put("/:id", 
-    middlewareController.verifyToken,
-    middlewareController.verifyTokenAndRole(['storekeeper']),
+    // middlewareController.verifyToken,
+    // middlewareController.verifyTokenAndRole(['storekeeper']),
     supplierController.updateSupplier);
 supplierRoutes.delete("/:id", 
-    middlewareController.verifyToken,
-    middlewareController.verifyTokenAndRole(['storekeeper']),
+    // middlewareController.verifyToken,
+    // middlewareController.verifyTokenAndRole(['storekeeper']),
     supplierController.deleteSupplier);
 supplierRoutes.get("/:id", 
-    middlewareController.verifyToken,
-    middlewareController.verifyTokenAndRole(['storekeeper']),
+    // middlewareController.verifyToken,
+    // middlewareController.verifyTokenAndRole(['storekeeper']),
     supplierController.getSupplierById);
 
 module.exports = supplierRoutes;
